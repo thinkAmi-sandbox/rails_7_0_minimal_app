5849826b19236a27fa73c10d30725ce88e641266
thinkAmi <dev.thinkami+git@gmail.com>
2023-10-04 19:56:11 +0900

    add example: overwrite trait with rspec-parameterized

--- spec/models/area_spec.rb
+++ spec/models/area_spec.rb
@@ -10,5 +10,42 @@
 require 'rails_helper'
 
 RSpec.describe Area, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'rspec-parameterized' do
+    describe 'overwrite trait' do
+      context 'without trait' do
+        let!(:static_area) { create(:area) }
+
+        it 'static trait' do
+          expect(static_area.name).to eq('日本')
+        end
+      end
+
+      context 'static trait' do
+        let!(:static_area) { create(:area, :tokyo) }
+
+        it 'static trait' do
+          expect(static_area.name).to eq('東京都')
+        end
+      end
+
+      context 'dynamic trait with rspec-parameterized' do
+        let(:trait_symbol) { nil }
+        let!(:dynamic_area) { create(:area, trait_symbol) }
+
+        where(:trait_symbol, :result) do
+          [
+            [:tokyo, '東京都'],
+            [:okinawa, '沖縄県'],
+            [nil, '日本'],  # traitを使いたくない場合は nil を渡す
+          ]
+        end
+
+        with_them do
+          it 'dynamic trait' do
+            expect(dynamic_area.name).to eq(result)
+          end
+        end
+      end
+    end
+  end
 end
